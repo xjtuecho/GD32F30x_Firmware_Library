425dcc5ad0f9683e2b8df1431aa896877eb51b69
Echo <echo.xjtu@gmail.com>
2021-12-05 17:58:43 +0800

    USART synchronous mode  works well.

--- Examples/USART/Synchronous/main.c
+++ Examples/USART/Synchronous/main.c
@@ -36,7 +36,7 @@ OF SUCH DAMAGE.
 */
 
 #include "gd32f30x.h"
-#include "gd32f307c_eval.h"
+#include "gd32f303c_eval.h"
 #include <stdio.h>
 
 #define txbuffer_size1   (countof(txbuffer1) - 1)
@@ -44,8 +44,8 @@ OF SUCH DAMAGE.
 #define DYMMY_BYTE       0x00
 #define countof(a)       (sizeof(a) / sizeof(*(a)))
 
-uint8_t txbuffer1[] = "USART synchronous example: USART0 -> SPI0 using TXE and RXNE Flags";
-uint8_t txbuffer2[] = "USART synchronous example: SPI0 -> USART0 using TXE and RXNE Flags";
+uint8_t txbuffer1[] = "USART synchronous example: USART1 -> SPI0 using TXE and RXNE Flags\r\n";
+uint8_t txbuffer2[] = "USART synchronous example: SPI0 -> USART1 using TXE and RXNE Flags\r\n";
 uint8_t rxbuffer1[txbuffer_size2];
 uint8_t rxbuffer2[txbuffer_size1];
 __IO uint8_t data_read1 = txbuffer_size2;
@@ -55,95 +55,6 @@ __IO uint8_t tx_counter2 = 0, rx_counter2 = 0;
 ErrStatus state1 = ERROR;
 ErrStatus state2 = ERROR;
 
-void usart_config(void);
-void spi_config(void);
-void led_init(void);
-ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint16_t length);
-
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
-{
-    /* initialize leds */
-    led_init();
-    /* turn off LED2~5 */
-    gd_eval_led_off(LED2);
-    gd_eval_led_off(LED3);
-    gd_eval_led_off(LED4);
-    gd_eval_led_off(LED5);
-
-    /* configure USART */
-    usart_config();
-
-    /* configure SPI */
-    spi_config();
-    
-    while(data_read2--){
-        while(RESET == usart_flag_get(USART0, USART_FLAG_TBE)){
-        }
-        /* write one byte in the USART0 data register */
-        usart_data_transmit(USART0, txbuffer1[tx_counter1++]);
-        /* wait until end of transmit */
-        while(RESET == usart_flag_get(USART0, USART_FLAG_TC)){
-        }
-        /* wait the byte is entirely received by SPI0 */  
-        while(RESET == spi_i2s_flag_get(SPI0, SPI_FLAG_RBNE)){
-        }
-        /* store the received byte in the rxbuffer2 */
-        rxbuffer2[rx_counter2++] = spi_i2s_data_receive(SPI0);
-    }
-
-    /* clear the USART0 data register */
-    usart_data_receive(USART0);
-
-    while(data_read1--){
-        /* wait until end of transmit */
-        while(RESET == spi_i2s_flag_get(SPI0, SPI_FLAG_TBE)){
-        }
-        /* write one byte in the SPI0 transmit data register */
-        spi_i2s_data_transmit(SPI0, txbuffer2[tx_counter2++]);
-
-        /* send a dummy byte to generate clock to slave */ 
-        usart_data_transmit(USART0, DYMMY_BYTE);
-        /* wait until end of transmit */
-        while(RESET == usart_flag_get(USART0, USART_FLAG_TC)){
-        }
-        /* wait the byte is entirely received by USART0 */
-        while(RESET == usart_flag_get(USART0, USART_FLAG_RBNE)){
-        }
-        /* store the received byte in the rxbuffer1 */
-        rxbuffer1[rx_counter1++] = usart_data_receive(USART0);
-    }
-  
-    /* check the received data with the send ones */
-    state1 = memory_compare(txbuffer1, rxbuffer2, txbuffer_size1);
-    state2 = memory_compare(txbuffer2, rxbuffer1, txbuffer_size2);
-    
-    if(SUCCESS == state1){
-        /* if the data transmitted from USART0 and received by SPI0 are the same */
-        gd_eval_led_on(LED2);
-        gd_eval_led_on(LED4);
-    }else{
-        /* if the data transmitted from USART0 and received by SPI0 are not the same */
-        gd_eval_led_off(LED2);
-        gd_eval_led_off(LED4);
-    }
-    if(SUCCESS == state2){
-        /* if the data transmitted from SPI0 and received by USART0 are the same */
-        gd_eval_led_on(LED3);
-        gd_eval_led_on(LED5);
-    }else{
-        /* if the data transmitted from SPI0 and received by USART0 are not the same */
-        gd_eval_led_off(LED3);
-        gd_eval_led_off(LED5);
-    }
-    while(1){
-    }
-}
 /*!
     \brief      configure USART
     \param[in]  none
@@ -154,21 +65,27 @@ void usart_config(void)
 {
     rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_USART0);
+    rcu_periph_clock_enable(RCU_USART1);
     rcu_periph_clock_enable(RCU_AF);
+
     /* configure USART Tx as alternate function push-pull */
-    gpio_init(GPIOA,GPIO_MODE_AF_PP,GPIO_OSPEED_50MHZ,GPIO_PIN_9|GPIO_PIN_8);
-    gpio_init(GPIOA,GPIO_MODE_IN_FLOATING,GPIO_OSPEED_50MHZ,GPIO_PIN_10);
+    gpio_init(GPIOA,GPIO_MODE_AF_PP,GPIO_OSPEED_50MHZ,GPIO_PIN_2 | GPIO_PIN_4 | GPIO_PIN_9);
+    gpio_init(GPIOA,GPIO_MODE_IN_FLOATING,GPIO_OSPEED_50MHZ,GPIO_PIN_3);
+
     /* configure USART synchronous mode */
-    usart_synchronous_clock_enable(USART0);
-    usart_synchronous_clock_config(USART0, USART_CLEN_EN, USART_CPH_2CK, USART_CPL_HIGH);
+    usart_synchronous_clock_enable(USART1);
+    usart_synchronous_clock_config(USART1, USART_CLEN_EN, USART_CPH_2CK, USART_CPL_HIGH);
     
     usart_baudrate_set(USART0, 115200);
+    usart_baudrate_set(USART1, 115200);
     /* configure USART transmitter */
     usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
+    usart_transmit_config(USART1, USART_TRANSMIT_ENABLE);
     /* configure USART receiver */
-    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
+    usart_receive_config(USART1, USART_RECEIVE_ENABLE);
     /* enable USART */
     usart_enable(USART0);
+    usart_enable(USART1);
 }
 
 /*!
@@ -203,20 +120,6 @@ void spi_config(void)
 }
 
 /*!
-    \brief      initialize leds
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_init(void)
-{
-    gd_eval_led_init(LED2);
-    gd_eval_led_init(LED3);
-    gd_eval_led_init(LED4);
-    gd_eval_led_init(LED5);
-}
-
-/*!
     \brief      memory compare function
     \param[in]  src: source data
     \param[in]  dst: destination data
@@ -232,4 +135,89 @@ ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint16_t length)
         }
     }
     return SUCCESS;
+}
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* configure USART */
+    usart_config();
+
+    /* configure SPI */
+    spi_config();
+    
+	printf("%s", txbuffer1);
+    while(data_read2--){
+        while(RESET == usart_flag_get(USART1, USART_FLAG_TBE)){
+        }
+        /* write one byte in the USART1 data register */
+        usart_data_transmit(USART1, txbuffer1[tx_counter1++]);
+        /* wait until end of transmit */
+        while(RESET == usart_flag_get(USART1, USART_FLAG_TC)){
+        }
+        /* wait the byte is entirely received by SPI0 */  
+        while(RESET == spi_i2s_flag_get(SPI0, SPI_FLAG_RBNE)){
+        }
+        /* store the received byte in the rxbuffer2 */
+        rxbuffer2[rx_counter2++] = spi_i2s_data_receive(SPI0);
+    }
+
+	printf("%s", txbuffer2);
+    /* clear the USART1 data register */
+    usart_data_receive(USART1);
+
+    while(data_read1--){
+        /* wait until end of transmit */
+        while(RESET == spi_i2s_flag_get(SPI0, SPI_FLAG_TBE)){
+        }
+        /* write one byte in the SPI0 transmit data register */
+        spi_i2s_data_transmit(SPI0, txbuffer2[tx_counter2++]);
+
+        /* send a dummy byte to generate clock to slave */ 
+        usart_data_transmit(USART1, DYMMY_BYTE);
+        /* wait until end of transmit */
+        while(RESET == usart_flag_get(USART1, USART_FLAG_TC)){
+        }
+        /* wait the byte is entirely received by USART1 */
+        while(RESET == usart_flag_get(USART1, USART_FLAG_RBNE)){
+        }
+        /* store the received byte in the rxbuffer1 */
+        rxbuffer1[rx_counter1++] = usart_data_receive(USART1);
+    }
+  
+    /* check the received data with the send ones */
+    state1 = memory_compare(txbuffer1, rxbuffer2, txbuffer_size1);
+    state2 = memory_compare(txbuffer2, rxbuffer1, txbuffer_size2);
+    
+    if(SUCCESS == state1){
+        /* if the data transmitted from USART1 and received by SPI0 are the same */
+		printf("data transmitted from USART1 and received by SPI0 are the same\r\n");
+    }else{
+        /* if the data transmitted from USART1 and received by SPI0 are not the same */
+		printf("data transmitted from USART1 and received by SPI0 are not the same\r\n");
+    }
+    if(SUCCESS == state2){
+        /* if the data transmitted from SPI0 and received by USART1 are the same */
+		printf("data transmitted from SPI0 and received by USART1 are the same\r\n");
+    }else{
+        /* if the data transmitted from SPI0 and received by USART1 are not the same */
+		printf("data transmitted from SPI0 and received by USART1 are not the same\r\n");
+    }
+
+    while(1){
+    }
+}
+
+
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART0, (uint8_t)ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
 }
