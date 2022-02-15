d85f154570630d82914841669ff98947f66a0803
Echo <echo.xjtu@gmail.com>
2022-02-15 14:58:25 +0800

    WWDGT demo works.

--- Examples/WWDGT/WWDGT_delay_feed/main.c
+++ Examples/WWDGT/WWDGT_delay_feed/main.c
@@ -1,6 +1,6 @@
 /*!
     \file    main.c
-    \brief   WWDGT delay feed demo 
+    \brief   WWDGT delay feed demo
 
     \version 2017-02-10, V1.0.0, firmware for GD32F30x
     \version 2018-10-10, V1.1.0, firmware for GD32F30x
@@ -11,27 +11,27 @@
 /*
     Copyright (c) 2020, GigaDevice Semiconductor Inc.
 
-    Redistribution and use in source and binary forms, with or without modification, 
+    Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
        list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
        and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
        specific prior written permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
@@ -39,7 +39,6 @@ OF SUCH DAMAGE.
 #include "gd32f303c_eval.h"
 #include <stdio.h>
 
-uint16_t wwdgtPeriod = 26;
 volatile uint32_t sysTickTimer = 0;
 
 // delays number of tick Systicks
@@ -71,8 +70,8 @@ void systick_config(void)
 int main(void)
 {
     gd_eval_com_init(EVAL_COM1);
-	systick_config();
-    Delay(150);
+    systick_config();
+    Delay(1000);
 
     /* check if the system has resumed from WWDGT reset */
     if(RESET != rcu_flag_get(RCU_FLAG_WWDGTRST)){
@@ -95,21 +94,12 @@ int main(void)
      */
     wwdgt_config(127, 80, WWDGT_CFG_PSC_DIV8);
     wwdgt_enable();
-    wwdgt_interrupt_enable();
 
     while(1){
-        /* insert 26 ms delay */
-        Delay(wwdgtPeriod);
-        /* update WWDGT counter */
+      //Delay(14);
+        Delay(28);
+      //Delay(42);
         wwdgt_counter_update(127);
-    }
-}
-
-void WWDGT_IRQHandler(void)
-{
-    if(SET == wwdgt_flag_get()){
-        printf("WWDGT reset, wwdgtPeriod=%d...\r\n", wwdgtPeriod);
-        wwdgt_flag_clear();
     }
 }
 
