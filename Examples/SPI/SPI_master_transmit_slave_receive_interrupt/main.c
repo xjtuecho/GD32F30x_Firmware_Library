57adbc9d6766097d389dc340b3984d86605e4cdc
Echo <echo.xjtu@gmail.com>
2022-03-15 15:13:19 +0800

    complete SPI_master_transmit_slave_receive_interrupt demo.

--- Examples/SPI/SPI_master_transmit_slave_receive_interrupt/main.c
+++ Examples/SPI/SPI_master_transmit_slave_receive_interrupt/main.c
@@ -1,6 +1,6 @@
 /*!
     \file    main.c
-    \brief   master send and slave receive data use interrupt mode 
+    \brief   master send and slave receive data use interrupt mode
 
     \version 2017-02-10, V1.0.0, firmware for GD32F30x
     \version 2018-10-10, V1.1.0, firmware for GD32F30x
@@ -11,94 +11,49 @@
 /*
     Copyright (c) 2020, GigaDevice Semiconductor Inc.
 
-    Redistribution and use in source and binary forms, with or without modification, 
+    Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
        list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
        and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
        specific prior written permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
 #include "gd32f30x.h"
-#include "gd32f30x_it.h"
-#include "gd32f307c_eval.h"
+#include "gd32f303c_eval.h"
+#include <stdio.h>
 
-#define arraysize                      10
-#define SET_SPI0_NSS_HIGH              gpio_bit_set(GPIOA,GPIO_PIN_3);
-#define SET_SPI0_NSS_LOW               gpio_bit_reset(GPIOA,GPIO_PIN_3);
+#define arraysize                    10
+#define SET_SPI0_NSS_HIGH()          gpio_bit_set(GPIOA,GPIO_PIN_4);
+#define SET_SPI0_NSS_LOW()           gpio_bit_reset(GPIOA,GPIO_PIN_4);
+
 __IO uint32_t send_n = 0, receive_n = 0;
 uint8_t spi0_send_array[arraysize] = {0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA};
-uint8_t spi2_receive_array[arraysize];
-ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint8_t length);
+uint8_t spi1_receive_array[arraysize];
 
-void rcu_config(void);
-void gpio_config(void);
-void spi_config(void);
-
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint8_t length)
 {
-    /* init led2 */
-    gd_eval_led_init(LED2);
-
-    /* NVIC config */
-    nvic_priority_group_set(NVIC_PRIGROUP_PRE1_SUB3);
-    nvic_irq_enable(SPI0_IRQn,1,1);
-    nvic_irq_enable(SPI2_IRQn,0,1);
-
-    /* peripheral clock enable */
-    rcu_config();
-    /* GPIO config */
-    gpio_config();
-    /* SPI config */
-    spi_config();
-
-    SET_SPI0_NSS_HIGH
-
-    /* SPI int enable */
-    spi_i2s_interrupt_enable(SPI0, SPI_I2S_INT_TBE);
-    spi_i2s_interrupt_enable(SPI2, SPI_I2S_INT_RBNE);
-
-    SET_SPI0_NSS_LOW
-
-    /* SPI enable */
-    spi_enable(SPI2);
-    spi_enable(SPI0);
-
-    /* wait transmit complete */
-    while(receive_n < arraysize);
-
-    SET_SPI0_NSS_HIGH
-
-    /* compare receive data with send data */
-    if(memory_compare(spi2_receive_array, spi0_send_array, arraysize))
-        gd_eval_led_on(LED2);
-    else
-        gd_eval_led_off(LED2);
-
-    while(1);
-   
+    while (length--){
+        if (*src++ != *dst++)
+            return ERROR;
+    }
+    return SUCCESS;
 }
 
 /*!
@@ -110,9 +65,9 @@ int main(void)
 void rcu_config(void)
 {
     rcu_periph_clock_enable(RCU_GPIOA);
-    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_GPIOB);
     rcu_periph_clock_enable(RCU_SPI0);
-    rcu_periph_clock_enable(RCU_SPI2);
+    rcu_periph_clock_enable(RCU_SPI1);
     rcu_periph_clock_enable(RCU_AF);
 }
 
@@ -124,15 +79,13 @@ void rcu_config(void)
 */
 void gpio_config(void)
 {
-    /* SPI0 GPIO config:SCK/PA5, MOSI/PA7 */
+    /* SPI0 GPIO config:NSS/PA4, SCK/PA5, MOSI/PA7 */
     gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_5 | GPIO_PIN_7);
-    /* config PA3 as SPI0_NSS */
-    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_3);
+    /* PA4 as NSS */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
 
-    gpio_pin_remap_config(GPIO_SPI2_REMAP,ENABLE);
-    /* SPI2 GPIO config: NSS/PA4, SCK/PC10, MISO/PC11 */
-    gpio_init(GPIOC, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10 |GPIO_PIN_11);
-    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
+    /* SPI1 GPIO config: NSS/PB12, SCK/PB13, MISO/PB14 */
+    gpio_init(GPIOB, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14);
 }
 
 /*!
@@ -143,38 +96,111 @@ void gpio_config(void)
 */
 void spi_config(void)
 {
-    spi_parameter_struct  spi_init_struct;
+    spi_parameter_struct spi_init_struct;
 
     /* SPI0 parameter config */
     spi_init_struct.trans_mode           = SPI_TRANSMODE_BDTRANSMIT;
     spi_init_struct.device_mode          = SPI_MASTER;
     spi_init_struct.frame_size           = SPI_FRAMESIZE_8BIT;
-    spi_init_struct.clock_polarity_phase = SPI_CK_PL_HIGH_PH_2EDGE;
+    spi_init_struct.clock_polarity_phase = SPI_CK_PL_LOW_PH_1EDGE;
     spi_init_struct.nss                  = SPI_NSS_SOFT;
-    spi_init_struct.prescale             = SPI_PSC_8;
+    spi_init_struct.prescale             = SPI_PSC_32;
     spi_init_struct.endian               = SPI_ENDIAN_MSB;
     spi_init(SPI0, &spi_init_struct);
 
-    /* SPI2 parameter config */
+    /* SPI1 parameter config */
     spi_init_struct.trans_mode  = SPI_TRANSMODE_BDRECEIVE;
     spi_init_struct.device_mode = SPI_SLAVE;
     spi_init_struct.nss         = SPI_NSS_HARD;
-    spi_init(SPI2, &spi_init_struct);
+    spi_init(SPI1, &spi_init_struct);
+}
+
+
+/*!
+    \brief      this function handles SPI0 Handler exception
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void SPI0_IRQHandler(void)
+{
+    if(RESET != spi_i2s_interrupt_flag_get(SPI0, SPI_I2S_INT_FLAG_TBE)){
+        /* send data */
+        while(RESET == spi_i2s_flag_get(SPI0, SPI_FLAG_TBE));
+        spi_i2s_data_transmit(SPI0, spi0_send_array[send_n++]);
+
+        if(arraysize == send_n){
+            spi_i2s_interrupt_disable(SPI0, SPI_I2S_INT_TBE);
+        }
+    }
 }
 
 /*!
-    \brief      memory compare function
-    \param[in]  src: source data pointer
-    \param[in]  dst: destination data pointer
-    \param[in]  length: the compare data length
+    \brief      this function handles SPI1 Handler exception
+    \param[in]  none
     \param[out] none
-    \retval     ErrStatus: ERROR or SUCCESS
+    \retval     none
 */
-ErrStatus memory_compare(uint8_t* src, uint8_t* dst, uint8_t length) 
+void SPI1_IRQHandler(void)
 {
-    while (length--){
-        if (*src++ != *dst++)
-            return ERROR;
-    }
-    return SUCCESS;
+    if(RESET != spi_i2s_interrupt_flag_get(SPI1,SPI_I2S_INT_FLAG_RBNE))
+        spi1_receive_array[receive_n++] = spi_i2s_data_receive(SPI1);
+}
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    gd_eval_com_init(EVAL_COM1);
+    usart_interrupt_enable(USART0, USART_INT_RBNE);
+    nvic_irq_enable(USART0_IRQn, 0, 0);
+
+    /* NVIC config */
+    nvic_priority_group_set(NVIC_PRIGROUP_PRE1_SUB3);
+    nvic_irq_enable(SPI0_IRQn,1,1);
+    nvic_irq_enable(SPI1_IRQn,0,1);
+
+    /* peripheral clock enable */
+    rcu_config();
+    /* GPIO config */
+    gpio_config();
+    /* SPI config */
+    spi_config();
+
+    SET_SPI0_NSS_HIGH();
+
+    /* SPI int enable */
+    spi_i2s_interrupt_enable(SPI0, SPI_I2S_INT_TBE);
+    spi_i2s_interrupt_enable(SPI1, SPI_I2S_INT_RBNE);
+
+    SET_SPI0_NSS_LOW();
+
+    /* SPI enable */
+    spi_enable(SPI1);
+    spi_enable(SPI0);
+
+    /* wait transmit complete */
+    while(receive_n < arraysize);
+
+    SET_SPI0_NSS_HIGH();
+
+    /* compare receive data with send data */
+    if(memory_compare(spi1_receive_array, spi0_send_array, arraysize))
+        printf("SPI1 received passed.\r\n");
+    else
+        printf("SPI1 received failed.\r\n");
+
+    while(1);
+}
+
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART0, (uint8_t)ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
 }
