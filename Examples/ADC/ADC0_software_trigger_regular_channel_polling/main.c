e032564efc40482dbb666dba01743f84b073e434
Echo <echo.xjtu@gmail.com>
2022-05-11 14:50:36 +0800

    update regular channel demo.

--- Examples/ADC/ADC0_software_trigger_regular_channel_polling/main.c
+++ Examples/ADC/ADC0_software_trigger_regular_channel_polling/main.c
@@ -11,104 +11,84 @@
 /*
     Copyright (c) 2020, GigaDevice Semiconductor Inc.
 
-    Redistribution and use in source and binary forms, with or without modification, 
+    Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
        list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
        and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
        specific prior written permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
 #include "gd32f30x.h"
-#include "systick.h"
 #include <stdio.h>
-#include "gd32f307c_eval.h"
+#include "gd32f303c_eval.h"
 
-__IO uint16_t adc_value[4];
+__IO uint16_t adc_value;
+__IO uint16_t adc_mV;
 
-void rcu_config(void);
-void gpio_config(void);
-void adc_config(void);
-uint16_t adc_channel_sample(uint8_t channel);
+volatile uint32_t sysTickTimer = 0;
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+void systick_config(void)
 {
-    /* system clocks configuration */
-    rcu_config();
-    /* systick configuration */
-    systick_config();  
-    /* GPIO configuration */
-    gpio_config();
-    /* ADC configuration */
-    adc_config();
-  
-    while(1){
-        adc_value[0]=adc_channel_sample(ADC_CHANNEL_1);
-        adc_value[1]=adc_channel_sample(ADC_CHANNEL_2);
-        adc_value[2]=adc_channel_sample(ADC_CHANNEL_3);
-        adc_value[3]=adc_channel_sample(ADC_CHANNEL_4);
+    /* setup systick timer for 1000Hz interrupts */
+    if (SysTick_Config(SystemCoreClock / 1000U)){
+        /* capture error */
+        while (1){
+        }
+    }
+    /* configure the systick handler priority */
+    NVIC_SetPriority(SysTick_IRQn, 0x00U);
+}
+
+void SysTick_Handler(void)
+{
+    sysTickTimer++;
+}
+
+//  delays number of tick Systicks (happens every 1 ms)
+void Delay(uint32_t dlyTicks)
+{
+    uint32_t curTicks;
+
+    curTicks = sysTickTimer;
+    while ((sysTickTimer - curTicks) < dlyTicks) {
+        __NOP();
     }
 }
 
-/*!
-    \brief      configure the different system clocks
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
 void rcu_config(void)
 {
+    rcu_periph_clock_enable(RCU_AF);
     /* enable GPIOA clock */
-    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOC);
     /* enable ADC clock */
     rcu_periph_clock_enable(RCU_ADC0);
     /* config ADC clock */
     rcu_adc_clock_config(RCU_CKADC_CKAPB2_DIV6);
 }
 
-/*!
-    \brief      configure the GPIO peripheral
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
 void gpio_config(void)
 {
     /* config the GPIO as analog mode */
-    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_10MHZ, GPIO_PIN_1);
-    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_10MHZ, GPIO_PIN_2);
-    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_10MHZ, GPIO_PIN_3);
-    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_10MHZ, GPIO_PIN_4);
+    gpio_init(GPIOC, GPIO_MODE_AIN, GPIO_OSPEED_10MHZ, GPIO_PIN_3);
 }
 
-/*!
-    \brief      configure the ADC peripheral
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
 void adc_config(void)
 {
     /* ADC mode config */
@@ -117,37 +97,53 @@ void adc_config(void)
     adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);
     /* ADC channel length config */
     adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 1U);
-    
+
     /* ADC trigger config */
-    adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL, ADC0_1_2_EXTTRIG_REGULAR_NONE); 
+    adc_external_trigger_source_config(ADC0, ADC_REGULAR_CHANNEL,
+        ADC0_1_2_EXTTRIG_REGULAR_NONE);
     /* ADC external trigger config */
     adc_external_trigger_config(ADC0, ADC_REGULAR_CHANNEL, ENABLE);
 
+    /* ADC regular channel config */
+    adc_regular_channel_config(ADC0, 0U, 13, ADC_SAMPLETIME_239POINT5);
+
     /* enable ADC interface */
     adc_enable(ADC0);
-    delay_1ms(1U);
+    Delay(1U);
     /* ADC calibration and reset calibration */
     adc_calibration_enable(ADC0);
 }
 
-
-/*!
-    \brief      ADC channel sample
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-uint16_t adc_channel_sample(uint8_t channel)
+int main(void)
 {
-    /* ADC regular channel config */
-    adc_regular_channel_config(ADC0, 0U, channel, ADC_SAMPLETIME_7POINT5);
-    /* ADC software trigger enable */
-    adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
+    gd_eval_com_init(EVAL_COM1);
+    /* configure systick */
+    systick_config();
+    /* system clocks configuration */
+    rcu_config();
+    /* GPIO configuration */
+    gpio_config();
+    /* ADC configuration */
+    adc_config();
 
-    /* wait the end of conversion flag */
-    while(!adc_flag_get(ADC0, ADC_FLAG_EOC));
-    /* clear the end of conversion flag */
-    adc_flag_clear(ADC0, ADC_FLAG_EOC);
-    /* return regular channel sample value */
-    return (adc_regular_data_read(ADC0));
+    while(1){
+        /* ADC software trigger enable */
+        adc_software_trigger_enable(ADC0, ADC_REGULAR_CHANNEL);
+        /* wait the end of conversion flag */
+        while(!adc_flag_get(ADC0, ADC_FLAG_EOC));
+        /* clear the end of conversion flag */
+        adc_flag_clear(ADC0, ADC_FLAG_EOC);
+        /* read regular channel sample value */
+        adc_value = adc_regular_data_read(ADC0);
+        adc_mV = adc_value*3300L>>12;
+        printf(" ADC=0x%04X the voltage is %dmV\r\n", adc_value, adc_mV);
+        Delay(1000);
+    }
+}
+
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART0, (uint8_t)ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
 }
