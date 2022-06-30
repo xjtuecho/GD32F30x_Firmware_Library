5c62d16cddd790b0ba35677ab02e8b567783c8a3
Echo <echo.xjtu@gmail.com>
2022-06-30 16:02:07 +0800

    update Deepsleep_wakeup_exti demo.

--- Examples/PMU/Deepsleep_wakeup_exti/main.c
+++ Examples/PMU/Deepsleep_wakeup_exti/main.c
@@ -11,85 +11,61 @@
 /*
     Copyright (c) 2020, GigaDevice Semiconductor Inc.
 
-    Redistribution and use in source and binary forms, with or without modification, 
+    Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
        list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
        and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
        specific prior written permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
 #include "gd32f30x.h"
-#include "gd32f307c_eval.h"
-#include "systick.h"
+#include "gd32f303c_eval.h"
 #include "main.h"
 
-void led_config(void);
-
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+void systick_config(void)
 {
-    /* systick config */
-    systick_config();
-    /* led config */
-    led_config();
-    /* clock enable */
-    rcu_periph_clock_enable(RCU_PMU);
-    /* wakeup key config */
-    gd_eval_key_init(KEY_WAKEUP, KEY_MODE_GPIO);
-    /* tamper key EXTI config */
-    gd_eval_key_init(KEY_TAMPER, KEY_MODE_EXTI);
-    /* press wakeup key to enter deepsleep mode and use tamper key generate a exti interrupt to wakeup mcu */
-    while(1){
-        if(RESET == gpio_input_bit_get(WAKEUP_KEY_GPIO_PORT, WAKEUP_KEY_PIN))
-            pmu_to_deepsleepmode(PMU_LDO_LOWPOWER, WFI_CMD);
+    /* setup systick timer for 1000Hz interrupts */
+    if (SysTick_Config(SystemCoreClock / 1000U)){
+        /* capture error */
+        while (1){
+        }
     }
+    /* configure the systick handler priority */
+    NVIC_SetPriority(SysTick_IRQn, 0x00U);
 }
 
-/*!
-    \brief      toggle the led
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_spark(void)
+void SysTick_Handler(void)
 {
     static __IO uint32_t timingdelaylocal = 0;
 
     if (timingdelaylocal != 0x00){
         /* all the leds on */
         if(timingdelaylocal < 200){
-            gd_eval_led_on(LED2);
-            gd_eval_led_on(LED3);
-            gd_eval_led_on(LED4);
-            gd_eval_led_on(LED5);
+            gpio_bit_reset(GPIOA, GPIO_PIN_8);
+            gpio_bit_reset(GPIOA, GPIO_PIN_15);
+            gpio_bit_set(GPIOC, GPIO_PIN_13);
         }else{
             /* all the leds off */
-            gd_eval_led_off(LED2);
-            gd_eval_led_off(LED3);
-            gd_eval_led_off(LED4);
-            gd_eval_led_off(LED5);
+            gpio_bit_set(GPIOA, GPIO_PIN_8);
+            gpio_bit_set(GPIOA, GPIO_PIN_15);
+            gpio_bit_reset(GPIOC, GPIO_PIN_13);
         }
         timingdelaylocal--;
     }else{
@@ -97,16 +73,56 @@ void led_spark(void)
     }
 }
 
-/*!
-    \brief      led config
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_config(void)
+void gpio_config(void)
 {
-    gd_eval_led_init(LED2);
-    gd_eval_led_init(LED3);
-    gd_eval_led_init(LED4);
-    gd_eval_led_init(LED5);
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_AF);
+
+    /* PA0 = KEY1 */
+    gpio_init(GPIOA, GPIO_MODE_IPU, GPIO_OSPEED_2MHZ,  GPIO_PIN_0);
+    /* PA1 = KEY2 */
+    gpio_init(GPIOA, GPIO_MODE_IPU, GPIO_OSPEED_2MHZ,  GPIO_PIN_1);
+
+    /* PA8  = LED3 */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ,  GPIO_PIN_8);
+    gpio_bit_reset(GPIOA, GPIO_PIN_8);
+
+    /* PA15 = LED4 */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ,  GPIO_PIN_15);
+    gpio_bit_reset(GPIOA, GPIO_PIN_15);
+    gpio_pin_remap_config(GPIO_SWJ_SWDPENABLE_REMAP, ENABLE);
+
+    /* PC13  = LED */
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP,   GPIO_OSPEED_2MHZ,  GPIO_PIN_13);
+    gpio_bit_set(GPIOC, GPIO_PIN_13);
+}
+
+void exti_config(void)
+{
+    nvic_irq_enable(EXTI1_IRQn, 2U, 0U);
+    /* connect key EXTI line to key GPIO pin */
+    gpio_exti_source_select(GPIO_PORT_SOURCE_GPIOA, GPIO_PIN_SOURCE_1);
+    /* configure key EXTI line */
+    exti_init(EXTI_1, EXTI_INTERRUPT, EXTI_TRIG_FALLING);
+    exti_interrupt_flag_clear(EXTI_1);
+}
+
+int main(void)
+{
+    systick_config();
+    gpio_config();
+	exti_config();
+    rcu_periph_clock_enable(RCU_PMU);
+    while(1){
+        if(RESET == gpio_input_bit_get(GPIOA, GPIO_PIN_0))
+            pmu_to_deepsleepmode(PMU_LDO_LOWPOWER, WFI_CMD);
+    }
+}
+
+void EXTI1_IRQHandler(void)
+{
+    if(SET == exti_interrupt_flag_get(EXTI_1)){
+        exti_interrupt_flag_clear(EXTI_1);
+    }
 }
