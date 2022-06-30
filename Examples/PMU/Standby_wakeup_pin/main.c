bafd1ff1ede49170af9614f9becbeaf5f4028ce2
Echo <echo.xjtu@gmail.com>
2022-06-30 17:44:11 +0800

    update Standby_wakeup_pin demo.

--- Examples/PMU/Standby_wakeup_pin/main.c
+++ Examples/PMU/Standby_wakeup_pin/main.c
@@ -11,74 +11,100 @@
 /*
     Copyright (c) 2020, GigaDevice Semiconductor Inc.
 
-    Redistribution and use in source and binary forms, with or without modification, 
+    Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
        list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
        and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
        specific prior written permission.
 
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
 OF SUCH DAMAGE.
 */
 
 #include "gd32f30x.h"
-#include <stdio.h>
-#include "gd32f307c_eval.h"
 
-void led_config(void);
-
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-int main(void)
+void systick_config(void)
 {
-    /* clock enable */
-    rcu_periph_clock_enable(RCU_PMU);
-    /* led configuration and turn on all led */
-    led_config();
-    gd_eval_led_on(LED3);
-    gd_eval_led_on(LED2);
-    gd_eval_led_on(LED4);
-    gd_eval_led_on(LED5);
-    /* tamper key configuration */
-    gd_eval_key_init(KEY_TAMPER, KEY_MODE_GPIO);
-    /* wakeup pin enable */
-    pmu_wakeup_pin_enable();
-    /* press tamper key to enter standby mode and use wakeup key to wakeup mcu */
-    while(1){
-        if(RESET == gpio_input_bit_get(TAMPER_KEY_GPIO_PORT, TAMPER_KEY_PIN)){
-            pmu_to_standbymode(WFI_CMD);
+    /* setup systick timer for 1000Hz interrupts */
+    if (SysTick_Config(SystemCoreClock / 1000U)){
+        /* capture error */
+        while (1){
         }
+    }
+    /* configure the systick handler priority */
+    NVIC_SetPriority(SysTick_IRQn, 0x00U);
+}
+
+void SysTick_Handler(void)
+{
+    static __IO uint32_t timingdelaylocal = 0;
+
+    if (timingdelaylocal != 0x00){
+        /* all the leds on */
+        if(timingdelaylocal < 200){
+            gpio_bit_reset(GPIOA, GPIO_PIN_8);
+            gpio_bit_reset(GPIOA, GPIO_PIN_15);
+            gpio_bit_set(GPIOC, GPIO_PIN_13);
+        }else{
+            /* all the leds off */
+            gpio_bit_set(GPIOA, GPIO_PIN_8);
+            gpio_bit_set(GPIOA, GPIO_PIN_15);
+            gpio_bit_reset(GPIOC, GPIO_PIN_13);
+        }
+        timingdelaylocal--;
+    }else{
+        timingdelaylocal = 400;
     }
 }
 
-/*!
-    \brief      configure led 
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_config(void)
+void gpio_config(void)
 {
-    gd_eval_led_init(LED2);
-    gd_eval_led_init(LED3);
-    gd_eval_led_init(LED4);
-    gd_eval_led_init(LED5);
+    rcu_periph_clock_enable(RCU_GPIOA);
+    rcu_periph_clock_enable(RCU_GPIOC);
+    rcu_periph_clock_enable(RCU_AF);
+
+    /* PA0 = KEY1 */
+    gpio_init(GPIOA, GPIO_MODE_IPU, GPIO_OSPEED_2MHZ,  GPIO_PIN_0);
+    /* PA1 = KEY2 */
+    gpio_init(GPIOA, GPIO_MODE_IPU, GPIO_OSPEED_2MHZ,  GPIO_PIN_1);
+
+    /* PA8  = LED3 */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ,  GPIO_PIN_8);
+    gpio_bit_reset(GPIOA, GPIO_PIN_8);
+
+    /* PA15 = LED4 */
+    gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_2MHZ,  GPIO_PIN_15);
+    gpio_bit_reset(GPIOA, GPIO_PIN_15);
+    gpio_pin_remap_config(GPIO_SWJ_SWDPENABLE_REMAP, ENABLE);
+
+    /* PC13  = LED */
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP,   GPIO_OSPEED_2MHZ,  GPIO_PIN_13);
+    gpio_bit_set(GPIOC, GPIO_PIN_13);
+}
+
+int main(void)
+{
+    systick_config();
+    gpio_config();
+    rcu_periph_clock_enable(RCU_PMU);
+    pmu_wakeup_pin_enable();
+    while(1){
+        if(RESET == gpio_input_bit_get(GPIOA, GPIO_PIN_1)){
+            pmu_to_standbymode(WFI_CMD);
+        }
+    }
 }
